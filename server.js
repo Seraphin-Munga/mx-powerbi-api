f7798fef13bdfe768261d7c7c2566a1271e1f9d8
Seraphin-Munga <37819167+Seraphin-Munga@users.noreply.github.com>
2022-03-24 15:56:19 +0200

    ping

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const express = require("express");
 const { sendStatus } = require("express/lib/response");
 
 const app = express();
-
-const jwt = require("jsonwebtoken");
-
+const OAuth2Strategy = require('../lib/strategy');
+const passport = require('passport-strategy')
 app.use(express.json());
 
 app.post("/api/validate-ping", authenicateToken, (req, res) => {
@@ -22,11 +21,32 @@ function authenicateToken(req, res, nex) {
 
   if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
-    if (err) return sendStatus(403);
-    req.token = token;
-    next();
-  });
+  passport.use(new OAuth2Strategy({
+    authorizationURL: 'https://www.example.com/as/authorization.oauth2',
+    tokenURL: `https://www.example.com/as/${token}`,
+    clientID: EXAMPLE_CLIENT_ID,
+    clientSecret: EXAMPLE_CLIENT_SECRET,
+    callbackURL: ""
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOne({ 'ping.id' : profile.id }, function (err, user) {
+        if (err) { return done(err); }
+        if (user) {
+            return done(null, user);
+        } else {
+            var newUser = new User();
+            newUser.ping.id    = profile.id;
+            newUser.ping.token = accessToken;
+            newUser.ping.name  = profile.displayName;
+            newUser.ping.email = profile.email;
+            newUser.save(function(err) {
+                if (err) { throw err; }
+                return done(null, newUser);
+            });
+        }
+    });
+  }
+));
 }
 
 app.listen(3000);
